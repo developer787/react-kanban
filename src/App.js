d9d162c20b0df8a0fd1cfeda4f5e1b8de9bcd874
developer787 <nodejs787@gmail.com>
2018-10-19 14:04:57 -0400

    Added buttons to Card component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import MessageBanner from './components/Message'
 import ToggleMessage from './components/ToggleMessage'
 import CardA from './components/CardA'
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <AppContainer>
 	    <MessageBanner />
 	    <ToggleMessage />
 	    <CardA />
-        </header>
-      </div>
+      </AppContainer>
     );
   }
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  width: 100%;
+  background: #456;
+  border: none;
+  display: flex;
+  flex-direction: column;
+  `
--- src/components/CardA.js
+++ src/components/CardA.js
@@ -9,19 +9,29 @@ const mapState = state=>({
 
 class CardA extends React.Component{
 	addItem(){
-	  alert("Item added")
+		alert("Item added")
 	}
 	render(){
 		const { items } = this.props
 		return(
 			<CardContainer>
 			<StyledCardA>
-			&lt;{"Card A"}&gt;
+			&lt;{"Backlog"}&gt;
 			</StyledCardA>
+			<StoryInput />
 			<StyledButton
-			  onClick={this.addItem}>
-			{"\u2795"}
+			onClick={this.addItem}>
+			{"+"}
 			</StyledButton>
+			<StyledCancel
+			onClick={this.addItem}>
+			{"x"}
+			</StyledCancel>
+			<StyledSubmit
+			placeholder={"add user story here..."}
+			onClick={this.addItem}>
+			{"Save"}
+			</StyledSubmit>
 			</CardContainer>
 
 		)
@@ -31,28 +41,70 @@ class CardA extends React.Component{
 
 export default connect(mapState)(CardA)
 
+const StoryInput = styled.input`
+    height: 34px;
+      background-color: #ddd;
+	    padding-left: 10px;
+	      font: 400 14px 'Source Sans Pro', 'sans-serif';
+		::-webkit-input-placeholder {
+		    color: red;
+		      }
+		::placeholder {
+			font: 400 14px 'Source Sans Pro', 'sans-serif';
+			  }
+			  `
 const CardContainer = styled.div`
-  padding: 0.5em;
-  width: 100%;
-    margin: 0.5em;
+  width: 90%;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
   background: #fff;
   border: none;
-  border-radius: 3px;
   `
 const StyledCardA = styled.div`
-  padding: 0.5em;
-  width: 90%;
-    margin: 0.5em;
-  color: ${props => props.inputColor || "palevioletred"};
-  background: papayawhip;
-  border: none;
-  border-radius: 3px;
-  `
+text-align: center;
+padding:6px;
+background: papayawhip;
+font-size: 6vmin;
+border: none;
+border-radius: 3px;
+`
 const StyledButton = styled.button`
-  background: #3CB371;
-  color: #fff;
-  border: none;
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  `
+background: #3CB371;
+color: #fff;
+border: none;
+width: 30px;
+height: 30px;
+line-height: 30px;
+text-align: center;
+font-size: 6vmin;
+font-weight: 900;
+border-radius: 50%;
+`
+
+const StyledSubmit = styled.button`
+background: #3CB371;
+color: #fff;
+border: none;
+height: 30px;
+line-height: 30px;
+text-align: center;
+font-size: 6vmin;
+font-weight: 900;
+border-radius: 3px;
+`
+const StyledCancel = styled.button`
+background: #B33C52;
+color: #fff;
+border: none;
+width: 30px;
+height: 30px;
+line-height: 26px;
+text-align: center;
+font-size: 6vmin;
+font-weight: 900;
+border-radius: 50%;
+`
